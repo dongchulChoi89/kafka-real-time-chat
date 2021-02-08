2add31a9dec986f45fa49ed56a7872fa9501287e
dongchulchoi <l77lucky@gmail.com>
2021-02-08 20:46:30 +0900

    initial setup

---
+++ src/main/java/com/choi/kafka/kafkarealtimechatApp/KafkaRealTimeChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.choi.kafka.kafkarealtimechatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealTimeChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRealTimeChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/choi/kafka/kafkarealtimechatApp/KafkaRealTimeChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choi.kafka.kafkarealtimechatApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealTimeChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
