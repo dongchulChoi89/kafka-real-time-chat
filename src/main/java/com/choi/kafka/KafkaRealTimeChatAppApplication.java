083ab69c9cf5019041f1815dc4711d63a3726484
dongchulchoi <l77lucky@gmail.com>
2021-02-09 21:28:35 +0900

    Consumer

--- src/main/java/com/choi/kafka/KafkaRealTimeChatAppApplication.java
+++ src/main/java/com/choi/kafka/KafkaRealTimeChatAppApplication.java
@@ -1,4 +1,4 @@
-package com.choi.kafka.kafkarealtimechatApp;
+package com.choi.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/choi/kafka/controller/KafkaController.java
+++ src/main/java/com/choi/kafka/controller/KafkaController.java
@@ -1,21 +1,31 @@
 package com.choi.kafka.controller;
 
+import com.choi.kafka.controller.MyTopicConsumer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 // this endpoint injects the KafkaTemplate configured earlier and sends a message to myTopic when a GET request is made to /kafka/produce
 @RestController
 public class KafkaController {
     private KafkaTemplate<String, String> template;
+    private MyTopicConsumer myTopicConsumer;
 
-    public KafkaController(KafkaTemplate<String, String> template) {
+    public KafkaController(KafkaTemplate<String, String> template, MyTopicConsumer myTopicConsumer) {
         this.template = template;
+        this.myTopicConsumer = myTopicConsumer;
     }
 
     @GetMapping("/kafka/produce")
     public void produce(@RequestParam String message) {
         template.send("myTopic", message);
+    }
+
+    @GetMapping("/kafka/messages")
+    public List<String> getMessages() {
+        return myTopicConsumer.getMessages();
     }
 }
